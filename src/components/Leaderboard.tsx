8415faeb489fb213a8efc42c599cca8e9ae2d5ad
Kenneth sunjaya <kennethsunjaya@gmail.com>
2025-03-20 00:12:00 +0700

    fix rank issue

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -13,10 +13,10 @@ interface LeaderboardProps {
 
 const Leaderboard: React.FC<LeaderboardProps> = ({setState, defaultDifficulty}) => {
   const [scores, setScores] = useState<Scoreboard[]>([]);
+  const [displayedScores, setDisplayedScores] = useState<Scoreboard[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [difficulty, setDifficulty] = useState<Difficulty>(defaultDifficulty);
   const [search, setSearch] = useState<string>("");
-  const [filteredScores, setFilteredScores] = useState<Scoreboard[]>([]);
 
   useEffect(() => {
     const fetchScores = async () => {
@@ -25,6 +25,10 @@ const Leaderboard: React.FC<LeaderboardProps> = ({setState, defaultDifficulty}) 
       data.sort((a: Scoreboard, b: Scoreboard) => a.time - b.time);
       data.forEach((score: Scoreboard, index: number) => score.rank = index + 1);
       setScores(data);
+      const displayedScores = data.filter((score: Scoreboard) => score.difficulty === difficulty).slice(0, 10);
+      displayedScores.sort((a: Scoreboard, b: Scoreboard) => a.time - b.time);
+      displayedScores.forEach((score: Scoreboard, index: number) => score.rank = index + 1);
+      setDisplayedScores(displayedScores);
       setLoading(false);
     };
 
@@ -32,14 +36,23 @@ const Leaderboard: React.FC<LeaderboardProps> = ({setState, defaultDifficulty}) 
   }, []);
 
   useEffect(() => {
-    if (search.length > 0) {
-      const filtered = scores.filter((score: Scoreboard) => score.name.toLowerCase().includes(search.toLowerCase()));
-      setFilteredScores(filtered);
+    if (scores.length > 0) {
+      const displayedScores = scores.filter((score: Scoreboard) => score.difficulty === difficulty).slice(0, 10);
+      displayedScores.sort((a: Scoreboard, b: Scoreboard) => a.time - b.time);
+      displayedScores.forEach((score: Scoreboard, index: number) => score.rank = index + 1);
+      setDisplayedScores(displayedScores);
     }
-  }, [search, difficulty]);
+  }, [difficulty])
+
+  useEffect(() => {
+    if (search.length > 0) {
+      const filtered = displayedScores.filter((score: Scoreboard) => score.name.toLowerCase().includes(search.toLowerCase()));
+      setDisplayedScores(filtered);
+    }
+  }, [search]);
 
   return (
-    <div className="fixed inset-0 bg-transparent w-full h-full backdrop-opacity-80 overflow-y-auto text-gray-900 dark:text-white flex flex-col items-center justify-center z-100">
+    <div className="fixed inset-0 bg-transparent w-full h-full backdrop-opacity-80 overflow-y-auto text-gray-900 dark:text-white flex flex-col items-center justify-start z-100">
       <div className="bg-white dark:bg-black px-5 pb-10 pt-5 rounded-md">
         <div className="flex justify-between items-start w-full text-2xl">
           <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Search" className=" outline-1 outline-gray-400 rounded-md text-sm px-2 w-40 focus:outline-blue-500 py-1" />
@@ -61,15 +74,7 @@ const Leaderboard: React.FC<LeaderboardProps> = ({setState, defaultDifficulty}) 
               </tr>
             </thead>
             <tbody>
-              {search.length > 0 ? filteredScores.filter((score: Scoreboard) => score.difficulty === difficulty).slice(0, 10).map((score: Scoreboard, index: number) => (
-                <tr key={index}>
-                  <td className="border px-4 py-2 text-center">{score.rank}</td>
-                  <td className="border px-4 py-2">{score.name}</td>
-                  <td className="border px-4 py-2">{formatTime(score.time)}</td>
-                  <td className="border px-4 py-2">{new Date(score.lastUpdated).toLocaleDateString()}</td>
-                </tr>
-              )) :
-              scores.filter((score: Scoreboard) => score.difficulty === difficulty).slice(0, 10).map((score: Scoreboard, index: number) => (
+              {displayedScores.filter((score: Scoreboard) => score.difficulty === difficulty).slice(0, 10).map((score: Scoreboard, index: number) => (
                 <tr key={index}>
                   <td className="border px-4 py-2 text-center">{score.rank}</td>
                   <td className="border px-4 py-2">{score.name}</td>
